a9fc551bf22f976894bd4fb2ab3473c2e6db22dd
Eitaro Fukamachi <e.arrows@gmail.com>
2021-08-29 15:53:49 +0900

    Use fukamachi/sbcl image for CI.

--- tests/run-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-exec sbcl --noinform --non-interactive \
-  --eval '(progn (write-line "Loading...") (ql:quickload (list :rove :mondo/tests) :silent t))' \
-  --eval '(or (rove:run :mondo/tests) (uiop:quit -1))'
